82338c3d4decfd73d40735c71f19052600d90541
Reynardo Tjhin <reychen26@gmail.com>
2022-09-30 13:57:55 +1000

    Update user interaction

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import pygame
 import draw
 import parse
 
+print("--------------------")
+print("Console for the game")
+print("--------------------")
+
 # initialise the pygame
 pygame.init()
 
@@ -14,6 +18,8 @@ color =(196, 211, 249)
 surface.fill(color)
 rect_color = (233,226,246)
 
+print("Creating a new window")
+
 # pygame.draw.rect(surface, rect_color, pygame.Rect(99, 66, 792, 528))
 # rect_border = (233,233,255)
 # pygame.draw.rect(surface, rect_border, pygame.Rect(99-10, 66-10, 792+20, 528+20),  10)
@@ -24,19 +30,18 @@ pygame.display.flip()
 levels = parse.parse_json()
 data = parse.read_map(levels[0]['path'])
 
-print(data)
-print(type(surface))
+print("Successfully parsing data for level 1")
 
 # draw the map
 my_map = draw.Map(data, surface)
 my_map.draw()
 
-print(my_map.map)
+print("Successfully created the map")
 
 # get the starting position of the player
 current_position = my_map.get_starting_position()
 
-print(current_position)
+print(f"The player's current position: {current_position}")
 
 # main loop
 run = True
@@ -56,29 +61,116 @@ while run:
     # user interaction with user input
     if keys[pygame.K_LEFT]:
         
-        # make a new string
-        result = ""
-        for j in range(len(my_map.map[current_position[0]])):
-            string = my_map.map[current_position[0]][j]
-            if (j == current_position[1] - 1):
-                result += "P"
-            elif (j == current_position[1]):
-                result += " "
-            else:
-                result += string
+        print("\tLEFT arrow is pressed")
+
+        # check for out of bounds
+        if (current_position[1] - 1 < 0):
+            print("WARNING: out of bounds")
+            continue
+
+        # check if the tile is valid
         
-        # assign to the current map data
-        my_map.map[current_position[0]] = result
+        # not out of bounds
+        # assign the new map after the user's moved
+        x = current_position[0]
+        y = current_position[1]
+        my_map.map[x] = my_map.map[x][:y-1] + "P" + " " + my_map.map[x][y+1:]
+
+        # do some animation of the dice
+
+        # update current position
+        current_position = (current_position[0], current_position[1] - 1)
+        print(f"\tPlayer's current position: {current_position}")
 
         # update the map's UI
-        my_map.read_data()
+        surface.fill(color)
+        my_map.draw()
 
-    if keys[pygame.K_RIGHT]:
-        pass
-    if keys[pygame.K_UP]:
-        pass
-    if keys[pygame.K_DOWN]:
-        pass
+    elif keys[pygame.K_RIGHT]:
+
+        print("\tRIGHT arrow is pressed")
+
+        # check for out of bounds
+        # 10 is the MAX column number
+        if (current_position[1] + 1 >= 10):
+            print("WARNING: out of bounds")
+            continue
+        
+        # check if tile is valid
+
+        # not out of bounds
+        x = current_position[0]
+        y = current_position[1]
+        my_map.map[x] = my_map.map[x][:y-1] + " " + "P" + my_map.map[x][y+1:]
+
+        # do some animation of the dice
+
+        # update current position
+        current_position = (current_position[0], current_position[1] + 1)
+
+        print(f"\tPlayer's current position: {current_position}")
+
+        # update the map's UI
+        surface.fill(color)
+        my_map.draw()
+
+    elif keys[pygame.K_UP]:
+        
+        print("\tUP arrow is pressed")
+
+        # check for out of bounds
+        # 10 is the MAX column number
+        if (current_position[0] - 1 < 0):
+            print("WARNING: out of bounds")
+            continue
+        
+        # check if tile is valid
+
+        # not out of bounds
+        x = current_position[0]
+        y = current_position[1]
+        my_map.map[x - 1] = my_map.map[x-1][:y] + "P" + my_map.map[x-1][y+1:]
+        my_map.map[x] = my_map.map[x][:y] + " " + my_map.map[x][y+1:]
+
+        # do some animation of the dice
+
+        # update current position
+        current_position = (current_position[0] - 1, current_position[1])
+
+        print(f"\tPlayer's current position: {current_position}")
+
+        # update the map's UI
+        surface.fill(color)
+        my_map.draw()
+    
+    elif keys[pygame.K_DOWN]:
+        
+        print("\tDOWN arrow is pressed")
+
+        # check for out of bounds
+        # 10 is the MAX column number
+        if (current_position[0] + 1 >= len(my_map.map)):
+            print("WARNING: out of bounds")
+            continue
+        
+        # check if tile is valid
+
+        # not out of bounds
+        x = current_position[0]
+        y = current_position[1]
+        my_map.map[x + 1] = my_map.map[x+1][:y] + "P" + my_map.map[x+1][y+1:]
+        my_map.map[x] = my_map.map[x][:y] + " " + my_map.map[x][y+1:]
+
+        # do some animation of the dice
+
+        # update current position
+        current_position = (current_position[0] + 1, current_position[1])
+
+        print(f"\tPlayer's current position: {current_position}")
+
+        # update the map's UI
+        surface.fill(color)
+        my_map.draw()
     
 
     my_map.read_data()
