93e95974bd8deb9b741ad1723756e6863c418bc0
Obiajulu2 <71581304+Obiajulu2@users.noreply.github.com>
2022-10-02 00:17:56 +1000

    add teleport sound

--- game.py
+++ game.py
@@ -13,7 +13,7 @@ RECT_COLOR = (233, 226, 246)
 
 SCREEN_WIDTH = 990
 SCREEN_HEIGHT = 660
-
+TELEPORT_SOUND = "./sounds/Retro Gun Laser SingleShot 01.wav"
 CLEAR_LEVEL_SOUND = "./sounds/Retro Success Melody 01 - sawtooth lead 1.wav"
 MAX_LEVEL = 5
 pygame.mixer.init()
@@ -232,6 +232,8 @@ class Game():
 
                 # hit the teleport
                 elif next_path_symbol == "T":
+                    pygame.mixer.music.load(TELEPORT_SOUND)
+                    pygame.mixer.music.play()
 
                     # get the dice to the position
                     my_map.map[x] = my_map.map[x][:y-1] + "P" + self.static_map[x][y] + my_map.map[x][y+1:]
@@ -328,6 +330,8 @@ class Game():
 
                 # hit the teleport
                 elif next_path_symbol == "T":
+                    pygame.mixer.music.load(TELEPORT_SOUND)
+                    pygame.mixer.music.play()
 
                     # get the dice to the position
                     my_map.map[x] = my_map.map[x][:y] + self.static_map[x][y] + "P" + my_map.map[x][y+2:]
@@ -424,7 +428,8 @@ class Game():
 
                 # hit the teleport
                 elif next_path_symbol == "T":
-                    
+                    pygame.mixer.music.load(TELEPORT_SOUND)
+                    pygame.mixer.music.play()
                     # get the dice to the position
                     my_map.map[x] = my_map.map[x][:y] + self.static_map[x][y] + my_map.map[x][y+1:]
                     my_map.map[x - 1] = my_map.map[x-1][:y] + "P" + my_map.map[x-1][y+1:]
@@ -525,7 +530,8 @@ class Game():
 
                 # hit the teleport
                 elif next_path_symbol == "T":
-                    
+                    pygame.mixer.music.load(TELEPORT_SOUND)
+                    pygame.mixer.music.play()
                     # get the dice to the position
                     my_map.map[x] = my_map.map[x][:y] + self.static_map[x][y] + my_map.map[x][y+1:]
                     my_map.map[x + 1] = my_map.map[x+1][:y] + "P" + my_map.map[x+1][y+1:]
